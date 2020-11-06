2c74d0709993ac0006d5ddceede5a180d2cd4168
ranriy <riyawasnik@gmail.com>
2020-11-06 22:34:25 +0530

    partial pie-chart

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from bulk_update_or_create import BulkUpdateOrCreateQuerySet
 
-# Create your models here.
+class Tag(models.Model):
+	name = models.CharField(max_length=50, primary_key=True)
+	unanswered_questions = models.IntegerField()
+	asked_questions = models.IntegerField()
+	percentage = models.DecimalField(max_digits=10,decimal_places=3)
+	
+	objects = BulkUpdateOrCreateQuerySet.as_manager()
--- tags/views.py
+++ tags/views.py
@@ -2,9 +2,29 @@ import requests
 
 from django.shortcuts import render
 
+from .models import Tag
+
 def index(request):
-	payload = {'order':'desc', 'sort':'popular', 'site':'stackoverflow'}
+	payload = {'order':'desc', 'sort':'popular', 'site':'stackoverflow', 'limit':50}
 	response = requests.get('https://api.stackexchange.com/2.2/tags', params=payload)
-	tag_items = response.json()["items"]
+	tag_items = response.json()['items']
+	tag_objects = []
+	print("HER")
+	for tag in tag_items:
+		payload = {'order':'desc', 'sort':'activity', 'tagged':tag['name'], 'site':'stackoverflow', 'filter':'total'}
+		response = requests.get('https://api.stackexchange.com/2.2/questions/unanswered', params=payload)
+		unanswered_questions = response.json()['total']
+		print(unanswered_questions)
+		tag_objects.append(Tag(name=tag['name'],asked_questions=tag['count'],unanswered_questions=unanswered_questions,percentage=0))
+	Tag.objects.bulk_update_or_create(tag_objects, ['asked_questions', 'unanswered_questions'], match_field='name')
 	print(tag_items)
 	return "ahs"#tag_items
+
+def display(request):
+	tags = Tag.objects.all()
+	labels = []
+	data = []
+	for tag in tags:
+		labels.append(tag.name)
+		data.append(float(tag.percentage))
+	return render(request, 'display.html', {'labels':labels, 'data':data})
